43ba3ca687159463eca69055b392ea014165a2c5
Andrew London <london.andrewd@gmail.com>
2016-03-30 21:24:19 -0400

    styled header and about

--- main.js
+++ main.js
@@ -2,18 +2,7 @@
 
 
 $(document).ready(function() {
-  $('.about-anchor').localScroll()
+  $.localScroll()
+  $('nav').sticky();
 
-
-//Help with fixed nav on scroll from:
-//http://stackoverflow.com/questions/22187317/jquery-positionfixed-navbar-by-scrolling-the-page
-    $(document).scroll(function () {
-        var scroll = $(this).scrollTop();
-        var scrollFire = $(".container").position();
-        if (scroll > scrollFire.top) {
-            $('nav ul').css({"position":"fixed","top":"0"});
-        } else {
-            $('nav ul').css({"position":"static","top":"auto"});
-        }
-    });
 });
